9303dedf8d7dc6a3bbab35dd3cb9c66025f6e539
Oleg6970 <53371159+Oleg6970@users.noreply.github.com>
2022-07-31 22:48:52 +0500

    header, hero

---
+++ js/index.js
@@ -0,0 +1,78 @@
+const params = {
+  btnClassName: "js-header-dropdown-btn",
+  dropClassName: "js-header-drop",
+  activeClassName: "is-active",
+  disabledClassName: "is-disabled"
+};
+
+function onDisable(evt) {
+  if (evt.target.classList.contains(params.disabledClassName)) {
+    evt.target.classList.remove(
+      params.disabledClassName,
+      params.activeClassName
+    );
+    evt.target.removeEventListener("animationend", onDisable);
+  }
+}
+
+function setMenuListener() {
+  document.body.addEventListener("click", (evt) => {
+    const activeElements = document.querySelectorAll(
+      `.${params.btnClassName}.${params.activeClassName}, .${params.dropClassName}.${params.activeClassName}`
+    );
+
+    if (
+      activeElements.length &&
+      !evt.target.closest(`.${params.activeClassName}`)
+    ) {
+      activeElements.forEach((current) => {
+        if (current.classList.contains(params.btnClassName)) {
+          current.classList.remove(params.activeClassName);
+        } else {
+          current.classList.add(params.disabledClassName);
+        }
+      });
+    }
+
+    if (evt.target.closest(`.${params.btnClassName}`)) {
+      const btn = evt.target.closest(`.${params.btnClassName}`);
+      const path = btn.dataset.path;
+      const drop = document.querySelector(
+        `.${params.dropClassName}[data-target="${path}"]`
+      );
+
+      btn.classList.toggle(params.activeClassName);
+
+      if (!drop.classList.contains(params.activeClassName)) {
+        drop.classList.add(params.activeClassName);
+        drop.addEventListener("animationend", onDisable);
+      } else {
+        drop.classList.add(params.disabledClassName);
+      }
+    }
+  });
+}
+
+setMenuListener();
+
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  // autoplay: {
+  //   delay: 5000,
+  // },
+  speed: 300,
+  // pagination: {
+  //   el: '.swiper-pagination',
+  //   type: "bullets",
+  //   clickable: true,
+  // },
+});
+
+const element = document.querySelector('.select');
+const choices = new Choices(element,{
+  searchEnabled: true,
+  placeholder: true,
+  placeholderValue: null,
+});
